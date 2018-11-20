1ea01ea3ccb2a794f32c146ab61b8281010d9d9b
David Zashkolny <davendiy@gmail.com>
2018-11-20 13:42:48 +0200

    add lab9, lab10

---
+++ dop_classes/multiple_integrals.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*-encoding: utf-8-*-
+
+# created: 20.11.18
+# by David Zashkolny
+# 2 course, comp math
+# Taras Shevchenko National University of Kyiv
+# email: davendiy@gmail.com
